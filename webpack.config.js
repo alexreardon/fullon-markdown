21ee2a3a27ca34917feb222d394505bc6716f478
Alex Reardon <areardon@atlassian.com>
2016-10-13 21:45:32 +1100

    adding production webpack concerns

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,38 @@ const webpack = require('webpack');
 const isDebug = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 8080;
 
+const appEntries = [
+    './src/entry',
+];
+
+const devEntries = [
+    `webpack-dev-server/client?http://0.0.0.0:${port}`, // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+];
+
+const productionPlugins = [
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false,
+        },
+        output: {
+            comments: false,
+        },
+    }),
+    // let libraries know to run in production mode
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+];
+
+const devPlugins = [new webpack.HotModuleReplacementPlugin()];
+
 module.exports = validate({
     devServer: {
         port,
     },
     entry: {
-        app: [
-            `webpack-dev-server/client?http://0.0.0.0:${port}`, // WebpackDevServer host and port
-            'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
-            './src/entry',
-        ],
+        app: isDebug ? devEntries.concat(appEntries) : appEntries,
     },
     devtool: isDebug ? 'cheap-module-eval-source-map' : undefined,
     output: {
@@ -47,7 +69,5 @@ module.exports = validate({
             },
         ],
     },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-    ],
+    plugins: isDebug ? devPlugins : productionPlugins,
 });
