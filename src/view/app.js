7065cb980ccbac4b468b34f64b1847380738bc6b
Alex Reardon <areardon@atlassian.com>
2016-10-14 14:13:12 +1100

    moving menu into consolidated location

--- src/view/app.js
+++ src/view/app.js
@@ -1,23 +1,10 @@
 import 'normalize.css';
 import React from 'react';
 import injectStyles from 'react-jss';
-import menu from 'react-burger-menu';
-import marked from 'marked';
-import slugg from 'slugg';
-import markdown from '../../details.md';
 import Details from './details';
 import Header from './header';
 import { colors, gutters } from './global-style';
-
-const tokens = marked.lexer(markdown);
-const headings = tokens
-    .filter(token => token.type === 'heading' && token.depth === 2)
-    .map((token) => {
-        const slug = slugg(token.text);
-        return <a key={slug} href={`#${slug}`}>{token.text}</a>;
-    });
-
-const Menu = menu.slide;
+import Menu from './menu';
 
 const global = `
     html {
@@ -63,47 +50,6 @@ const global = `
     }
 `;
 
-const menuStyles = {
-    bmBurgerButton: {
-        position: 'fixed',
-        width: '36px',
-        height: '30px',
-        right: '36px',
-        top: '36px',
-    },
-    bmBurgerBars: {
-        background: colors.white,
-    },
-    bmCrossButton: {
-        height: '24px',
-        width: '24px',
-    },
-    bmCross: {
-        background: colors.white,
-    },
-    bmMenuWrap: {
-        background: '#373a47',
-    },
-    bmMenu: {
-
-        padding: '2.5em 1.5em 0',
-        fontSize: '1.15em',
-
-        // stop overflow
-        height: 'auto',
-    },
-    bmMorphShape: {
-        fill: '#373a47',
-    },
-    bmItemList: {
-        color: '#b8b7ad',
-        padding: '0.8em',
-    },
-    bmOverlay: {
-        background: 'rgba(0, 0, 0, 0.3)',
-    },
-};
-
 const style = {
     container: {
         backgroundColor: colors.black,
@@ -116,10 +62,7 @@ const component = ({sheet: {classes}}) => (
         <style>
             {global}
         </style>
-        <Menu right styles={menuStyles}>
-            {headings}
-            <a href="http://en.stphils.org.au/">St Phils home page</a>
-        </Menu>
+        <Menu />
         <Header />
         <Details />
     </div>
---
+++ src/view/menu.js
@@ -0,0 +1,65 @@
+import 'normalize.css';
+import React from 'react';
+import menu from 'react-burger-menu';
+import marked from 'marked';
+import slugg from 'slugg';
+import markdown from '../../details.md';
+import { colors } from './global-style';
+
+const MenuSlide = menu.slide;
+
+const tokens = marked.lexer(markdown);
+const headings = tokens
+    .filter(token => token.type === 'heading' && token.depth === 2)
+    .map((token) => {
+        const slug = slugg(token.text);
+        return <a key={slug} href={`#${slug}`}>{token.text}</a>;
+    });
+
+const menuStyles = {
+    bmBurgerButton: {
+        position: 'fixed',
+        width: '36px',
+        height: '30px',
+        right: '36px',
+        top: '36px',
+    },
+    bmBurgerBars: {
+        background: colors.white,
+    },
+    bmCrossButton: {
+        height: '24px',
+        width: '24px',
+    },
+    bmCross: {
+        background: colors.white,
+    },
+    bmMenuWrap: {
+        background: '#373a47',
+    },
+    bmMenu: {
+
+        padding: '2.5em 1.5em 0',
+        fontSize: '1.15em',
+
+        // stop overflow
+        height: 'auto',
+    },
+    bmMorphShape: {
+        fill: '#373a47',
+    },
+    bmItemList: {
+        color: '#b8b7ad',
+        padding: '0.8em',
+    },
+    bmOverlay: {
+        background: 'rgba(0, 0, 0, 0.3)',
+    },
+};
+
+export default () => (
+    <MenuSlide right styles={menuStyles}>
+        {headings}
+        <a href="http://en.stphils.org.au/">St Phils home page</a>
+    </MenuSlide>
+);
