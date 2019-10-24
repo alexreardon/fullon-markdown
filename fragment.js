0cac8a1b492b7a44eee0fb924e81784b8ca8edb1
timhaywood <tim@haywood.org>
2019-10-24 15:13:07 +1100

    a11y: allow viewport scaling

--- fragment.js
+++ fragment.js
@@ -6,7 +6,7 @@ module.exports = () => `
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          content="width=device-width, initial-scale=1.0, maximum-scale=2.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Full on</title>
     <link href="${config.font.url}" rel="stylesheet">
