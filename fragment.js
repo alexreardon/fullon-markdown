a6dfc1180e7c4ad8c039da2934fb7a2c00b91269
timhaywood <tim@haywood.org>
2019-10-24 15:13:38 +1100

    change site title

--- fragment.js
+++ fragment.js
@@ -8,7 +8,7 @@ module.exports = () => `
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=2.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Full on</title>
+    <title>Full On Youth Camp</title>
     <link href="${config.font.url}" rel="stylesheet">
 </head>
 <body>
